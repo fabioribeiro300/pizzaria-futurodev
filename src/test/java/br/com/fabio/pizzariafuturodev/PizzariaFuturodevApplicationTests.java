0cd1a24e4291df2fa6a8d546517903fc2dadf11a
fabioribeiro300 <fabioribeiro300@gmail.com>
2022-10-14 21:10:47 -0300

    iniciando commit

---
+++ src/test/java/br/com/fabio/pizzariafuturodev/PizzariaFuturodevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabio.pizzariafuturodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaFuturodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
